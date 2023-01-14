4e43fe6c8ee2780199e2fe4956b0e4345bf27cef
JiaruiChen <chenjiaruisysu@gmail.com>
2023-01-14 13:29:45 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,3 @@
+Script1_Linear_Regression_Normalised_by_Covariates.R foldchange_exercise.csv metadata.csv
+Script2_Paired_Wilcoxon_Test.R exercise_before.csv exercise_after.csv
+Script3_Random_Forest_Model.R exercise_baseline.csv grouping.csv
