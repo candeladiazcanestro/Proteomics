cec9ee43a318bedebc4045071ba321fd285a0c30
JiaruiChen <chenjiaruisysu@gmail.com>
2023-01-14 13:46:19 +0800

    Update run.sh

--- example/run.sh
+++ example/run.sh
@@ -1,3 +1,4 @@
+######Simulated datasets were generated as examples for testing the following three R scripts
 Script1_Linear_Regression_Normalised_by_Covariates.R foldchange_exercise.csv metadata.csv
 Script2_Paired_Wilcoxon_Test.R exercise_before.csv exercise_after.csv
 Script3_Random_Forest_Model.R exercise_baseline.csv grouping.csv
